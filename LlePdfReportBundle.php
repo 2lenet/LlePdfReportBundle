87179bdffd1065b57ef646876274b751f0ea2de9
Sébastien HEITZMANN <2le@2le.net>
2015-05-12 06:26:03 +0200

    bundle creation

---
+++ LlePdfReportBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Lle\PdfReportBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class LlePdfReportBundle extends Bundle
+{
+
+
+
+}
